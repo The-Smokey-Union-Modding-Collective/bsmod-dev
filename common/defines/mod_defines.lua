c445692618752cd38031f77cef298e7ce4bef8dd
pbatt <pbattican@gmail.com>
2022-01-29 16:57:56 -0500

    set defines and localisation so there are 9 unit levels

--- common/defines/mod_defines.lua
+++ common/defines/mod_defines.lua
@@ -41,7 +41,10 @@ NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999		--vanilla=500
 NDefines.NMilitary.TRAINING_ATTRITION = 0.01	--vanilla=0.06
 NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false	--vanilla=true
 NDefines.NMilitary.GARRISON_ORDER_ARMY_CAP_FACTOR = 1	--vanilla=3
-NDefines.NMilitary.ARMY_TRAINING_FUEL_MULT = 0	--vanilla=1.0
+NDefines.NMilitary.ARMY_TRAINING_FUEL_MULT = 0	--vanilla=1.
+NDefines.NMilitary.UNIT_EXP_LEVELS = { 0.02, 0.15, 0.225, 0.35, 0.5, 0.7, 0.925, 1.15 } --vanilla={0.1, 0.3, 0.75, 0.9}
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.107 --vanilla=0.25
+
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 NDefines.NAir.AIR_WING_MAX_SIZE = 1600	--vanilla=1000
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
