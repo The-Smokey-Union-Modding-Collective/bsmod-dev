40905031cfe226a9ff0dbfa838c4fdad3e81bfcf
Rift Walker <vedran_k@outlook.com>
2022-05-31 14:17:00 +0200

    reverted div designer + gfx

--- common/defines/mod_defines.lua
+++ common/defines/mod_defines.lua
@@ -105,4 +105,3 @@ NDefines.NSupply.SUPPLY_FLOW_DROP_REDUCTION_AT_MAX_INFRA = 0.30 -- max infrastru
 NDefines.NSupply.SUPPLY_HUB_FULL_MOTORIZATION_BONUS = 2.2	-- The range bonus added to a fully motorized hub. This supply is added on top of the XXX_INITIAL_SUPPLY_FLOW defined above. # Vanilla = 2.2
 NDefines.NSupply.NAVAL_BASE_FLOW = 5.0 -- max output/input of a naval node is limited by this base value + additional ratio for each level # Vanilla = 15.0 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 3 -- Max height of regiments in division designer.
