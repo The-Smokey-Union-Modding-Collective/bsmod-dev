5d7a8521e896feb5dde4aea8993c22f145945572
Rift Walker <vedran_k@outlook.com>
2023-04-13 01:01:35 +0200

    large updates

--- common/defines/mod_defines.lua
+++ common/defines/mod_defines.lua
@@ -64,6 +64,32 @@ NDefines.NMilitary.LAND_AIR_COMBAT_STR_DAMAGE_MODIFIER = 0.026    -- vanilla 0.0
 NDefines.NMilitary.LAND_AIR_COMBAT_ORG_DAMAGE_MODIFIER = 0.026    -- vanilla 0.032
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.06 -- vanilla 0.4
 NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.14 -- vanilla 0.4
+NDefines.NMilitary.PIERCING_THRESHOLDS = {					-- Our piercing / their armor must be this value to deal damage fraction equal to the index in the array below [higher number = higher penetration]. If armor is 0, 1.00 will be returned.
+1.00,
+0.90,
+0.80,
+0.70,
+0.60,
+0.50,
+0.40,
+0.30,
+0.20,
+0.10,
+0.00, --there isn't much point setting this higher than 0
+}
+NDefines.NMilitary.PIERCING_THRESHOLD_DAMAGE_VALUES = {	-- 0 armor will always receive maximum damage (so add overmatching at your own peril). the system expects at least 2 values, with no upper limit.
+1.00,
+0.85,
+0.75,
+0.65,
+0.55,
+0.50,
+0.45,
+0.40,
+0.35,
+0.30,
+0.25,
+}
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 NDefines.NAir.AIR_WING_MAX_SIZE = 1600	--vanilla=1000
 NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
